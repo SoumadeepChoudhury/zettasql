f76b927b0108f5a5afecec941e7fec21274e0b08
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2023-02-15 20:27:01 +0530

    Added extra layer security

--- server/start.py
+++ server/start.py
@@ -2,10 +2,12 @@ import subprocess
 import platform
 import os
 import random
+if not os.path.exists(".NULL"):
+    os.mkdir(".NULL")
 PID = 0
 process=None
 PORT = str(random.randint(0,65336))
-if not os.path.exists("./log"):
+if not os.path.exists("./.log"):
     try:
         if platform.system() in ("Darwin","Linux"):
             process=subprocess.Popen(["python3","-m","http.server",PORT,"--directory",".NULL"],stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT)
@@ -18,7 +20,7 @@ if not os.path.exists("./log"):
         print(f"Error {e.args[0]}. Unable to connect to ZettaSQL serer.")
 
     if PID != 0:
-        with open("log",'wb') as file:
+        with open(".log",'wb') as file:
             import pickle
             try:
                 data=str(PID)+"%"+str(PORT)
--- server/stop.py
+++ server/stop.py
@@ -1,13 +1,13 @@
 import signal
 import os
 PID=0
-if os.path.exists("./log"):
-    with open("log",'rb') as file:
+if os.path.exists("./.log"):
+    with open(".log",'rb') as file:
         import pickle
         try:
             PID=int(str(pickle.load(file)).split("%")[0])
             os.kill(PID, signal.SIGKILL)
-            os.remove("./log")
+            os.remove("./.log")
         except:
             print("Unable to stop ZettaSQL server.")
 else:
